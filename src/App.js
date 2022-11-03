3e1a5144eeac0108f7be710cda48bb77885873ca
xeyzo <schneidergw@yahoo.com>
2022-11-03 09:23:23 -0700

    initialization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar.js";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Home from "./components/screens/Home.js";
+import Signup from "./components/screens/Signup.js";
+import Signin from "./components/screens/Signin.js";
+import Profile from "./components/screens/Profile.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/signin">
+            <Signin />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
 export default App;
+
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+ 
+
+const Navbar = () =>{
+    return(
+        <nav>
+        <div className="nav-wrapper white">
+          <a href ="/" className="brand-logo left">Instagram</a>
+          <ul id="nav-mobile" className="right ">
+            <li><a href ="/signin">Signin</a></li>
+            <li><a href ="/signup">Signup</a></li>
+            <li><a href ="/profile">Profile</a></li>
+          </ul>
+        </div>
+      </nav>    
+    )
+}
+
+export default Navbar
