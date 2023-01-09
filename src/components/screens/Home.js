2a42905ae0145434bcdd43fa93f750632dabd562
xeyzo <schneidergw@yahoo.com>
2023-01-09 05:38:02 -0800

    show all-post

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,59 +1,41 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 
 const Home = () => {
+  const [data, setData] = useState([])
+  useEffect(()=>{
+    fetch('/post/all-post',{
+      headers:{
+        "Authorization":"Bearer "+localStorage.getItem("jwt")
+      }
+    }).then(res => res.json())
+    .then(result => {
+      setData(result.data)
+    })
+  },[])
+
   return (
     <div className="home">
-      <div className="card home-card">
-        <h5 style={{padding:"5px"}}>Kipaklodon</h5>
-        <div className="card-image">
-          <img
-            src="https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHdhbHBhcGVyfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-            alt="img not found"
-          />
-        </div>
-        <div className="card-content">
-          <i className="material-icons" style={{ color: "red" }}>
-            favorite
-          </i>
-          <h6>title</h6>
-          <p>this is amazing post</p>
-          <input type="text" placeholder="add a comment" />
-        </div>
-      </div>
-      <div className="card home-card">
-        <h5>Kipaklodon</h5>
-        <div className="card-image">
-          <img
-            src="https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHdhbHBhcGVyfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-            alt="img not found"
-          />
-        </div>
-        <div className="card-content">
-          <i className="material-icons" style={{ color: "red" }}>
-            favorite
-          </i>
-          <h6>title</h6>
-          <p>this is amazing post</p>
-          <input type="text" placeholder="add a comment" />
-        </div>
-      </div>
-      <div className="card home-card">
-        <h5>Kipaklodon</h5>
-        <div className="card-image">
-          <img
-            src="https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHdhbHBhcGVyfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-            alt="img not found"
-          />
-        </div>
-        <div className="card-content">
-          <i className="material-icons" style={{ color: "red" }}>
-            favorite
-          </i>
-          <h6>title</h6>
-          <p>this is amazing post</p>
-          <input type="text" placeholder="add a comment" />
-        </div>
-      </div>
+      {
+        data.map(item=>(
+          <div className="card home-card">
+            <h5 style={{padding:"5px"}}>{item.postedBy.name}</h5>
+            <div className="card-image">
+              <img
+                src={item.photo}
+                alt="img not found"
+              />
+            </div>
+            <div className="card-content">
+              <i className="material-icons" style={{ color: "red" }}>
+                favorite
+              </i>
+              <h6>{item.title}</h6>
+              <p>{item.body}</p>
+              <input type="text" placeholder="add a comment" />
+            </div>
+          </div>
+        ))
+      } 
     </div>
   );
 };
